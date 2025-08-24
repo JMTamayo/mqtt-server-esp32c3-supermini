32ec1e519d667bf9f358c319b66b84383200fcdf
JMTamayo <jmtamayog23@gmail.com>
2025-08-24 12:12:54 -0500

    Implement configuration parameters for the application

---
+++ src/config/config/conf.cpp
@@ -0,0 +1,15 @@
+#include "conf.h"
+
+namespace config {
+
+const unsigned long DEFAULT_BAUD_RATE = 921600;
+
+const unsigned int LED_BRIGHTNESS = 255;
+
+const char *WIFI_SSID = "JMTamayo-2.4G";
+
+const char *WIFI_PASSWORD = "3ahY21me.31";
+
+const char *WIFI_HOSTNAME = "home-mqtt-broker";
+
+} // namespace config
